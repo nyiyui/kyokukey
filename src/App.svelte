ecbdd693eabd28f569c48f99d636900fd6428641
Ken Shibata <kenxshibata@gmail.com>
2022-01-15 14:33:23 -0500

    allow chaging target

--- src/App.svelte
+++ src/App.svelte
@@ -8,11 +8,11 @@
 	let wpmText = "";
 	let cpmText = "";
 	let errText = "";
+	let target = "hello world";
 
 	const dmp = new diff_match_path();
 	let elapsed: number = Date.now();
 	let lastChange: number = Date.now();
-	let target = "hello world\n";
 	$: {
 		if (text !== undefined && result !== undefined) {
 			lastChangeText = `${Date.now() - lastChange} ms`;
@@ -54,28 +54,31 @@
 	}
 </script>
 
+<svelte:head>
+	<title>Kyokukey Testing</title>
+	<meta name="robots" content="noindex, nofollow">
+	<html lang="en" />
+</svelte:head>
+
 <main>
 	<textarea bind:value={text}></textarea>
 	<div id="result" bind:this={result}>
 		<ins>insert</ins>
 		<del>delete</del>
 	</div>
-	<p>
-		<span contenteditable="true" bind:textContent={lastChangeText}></span> since last change
-	</p>
-	<p>
-		<span contenteditable="true" bind:textContent={elapsedText}></span> since start
-	</p>
-	<p>
-		<span contenteditable="true" bind:textContent={cpmText}></span> characters/min
-	</p>
-	<p>
-		<span contenteditable="true" bind:textContent={wpmText}></span> words/min
-	</p>
-	<p>
-		<span contenteditable="true" bind:textContent={errText}></span> % errors
-	</p>
+	<p><span contenteditable="true" bind:textContent={lastChangeText}></span> since last change</p>
+	<p><span contenteditable="true" bind:textContent={elapsedText}></span> since start</p>
+	<p><span contenteditable="true" bind:textContent={cpmText}></span> characters/min</p>
+	<p><span contenteditable="true" bind:textContent={wpmText}></span> words/min</p>
+	<p><span contenteditable="true" bind:textContent={errText}></span> % errors</p>
 	<input type="button" value="Reset" on:click={reset} />
+	<aside>
+		<details>
+			<summary>Settings</summary>
+			<label for="target">Target:</label>
+			<textarea id="target" bind:value={target}></textarea>
+		</details>
+	</aside>
 </main>
 
 <style>
